f0acdebc71791137318941c7f35c0ffa633d9cf6
Randy Consuegra <rsconsuegra95@gmail.com>
2023-02-15 01:05:28 -0500

    Add numerical model

--- amlcs/numerical_models/numerical_model.py
+++ amlcs/numerical_models/numerical_model.py
@@ -1,4 +1,17 @@
+import os
+
+
 class NumericalModel:
     def __init__(self, model_resolution: dict, variables_by_layers:dict) -> None:
         self.model_resolution:dict = model_resolution
         self.vars_by_layers:dict = variables_by_layers
+    
+    def set_time_integration(self, times):
+        nmonths = times[0]
+        days = times[1]
+        restart = times[2]
+        self.create_cls_instep_file(nmonths, days, restart)
+        self.create_cls_indyns_file()
+        os.system(
+            f"mv cls_instep.h {self.source_local}cls_instep.h; mv cls_indyns.h {self.source_local}cls_indyns.h; cd {self.source_local}/ ; sh compile.sh>out.txt;"
+        )
